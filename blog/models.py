2ac1c086416fbca3e2b38503d218d6c8f4343507
Haruka Asahi <ha.kapibarasan.lavender.0130@gmail.com>
2018-12-30 22:22:52 +0900

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,8 @@ class Post(models.Model):
         )
     published_date = models.DateTimeField(
         blank=True,null=True)
+#ポスト内容を保存するためのもの
+
 
 def publish(self):
     self.published_date = timezone.now()
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+#.modelsはこのviews.pyと同じディレクトリないにある models.pyのことを指す.
+from django.utils import timezone
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def post_list(request,Post):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+#post_list関数の定義.Postモデルからブログの記事を取り出してそのリストをテンプレートに渡す.
+#requestを引数にとり、クエリセットのデータをpostsで参照する.
+#それらの情報をrender関数でpost_list.htmlに組み込む.
+#引数の名前が'posts'、クエリセットで参照した引数の値がposts.
+
+#ブラウザはHTMLしか理解できないが、HTMLだけでは静的なページしか書けない.
+#でもpythonを用いれば動的なページも書ける.Djangoを使えばHTMLにPythonのコードを埋め込むことが可能.
