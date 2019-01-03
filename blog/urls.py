4b37e39fa2573f2abfa903510c06904894880708
Haruka Asahi <ha.kapibarasan.lavender.0130@gmail.com>
2019-01-03 17:37:59 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #整数値を変数pkに格納してviewへ渡す
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import Post
-#.modelsはこのviews.pyと同じディレクトリないにある models.pyのことを指す.
 from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+#.modelsはこのviews.pyと同じディレクトリないにある models.pyのことを指す.
 
-def post_list(request,Post):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
-    return render(request, 'blog/post_list.html', {'posts':posts})
-
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 #post_list関数の定義.Postモデルからブログの記事を取り出してそのリストをテンプレートに渡す.
 #requestを引数にとり、クエリセットのデータをpostsで参照する.
 #それらの情報をrender関数でpost_list.htmlに組み込む.
@@ -14,3 +14,7 @@ def post_list(request,Post):
 
 #ブラウザはHTMLしか理解できないが、HTMLだけでは静的なページしか書けない.
 #でもpythonを用いれば動的なページも書ける.Djangoを使えばHTMLにPythonのコードを埋め込むことが可能.
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
